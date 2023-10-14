7fb3e07cb80a176835e128f1ab20d540cb2a08c8
Nakamura Ryo <49283537+oyrar@users.noreply.github.com>
2023-10-15 04:33:31 +0900

    add rand

--- tutorial/project/rust/guessing_game/src/main.rs
+++ tutorial/project/rust/guessing_game/src/main.rs
@@ -1,7 +1,13 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    println!("The secret number is: {}", secret_number);
+
     println!("Pleasse input your guess.");
 
     let mut guess = String::new();
